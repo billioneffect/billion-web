bc76ccb18c8383b30d3c7aad38f99c3f3e99ce32
Alex Vargas <vlexaargas@gmail.com>
2016-11-11 18:31:30 -0500

    Add active flag to competitions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111093902) do
+ActiveRecord::Schema.define(version: 20161111232133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,13 +42,14 @@ ActiveRecord::Schema.define(version: 20161111093902) do
   add_index "competition_features", ["product_feature_id"], name: "index_competition_features_on_product_feature_id", using: :btree
 
   create_table "competitions", force: :cascade do |t|
-    t.string   "code_name",       default: "",   null: false
-    t.date     "start_date",                     null: false
-    t.date     "end_date",                       null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.boolean  "open_donation",   default: true, null: false
-    t.integer  "dollar_to_point", default: 1,    null: false
+    t.string   "code_name",       default: "",    null: false
+    t.date     "start_date",                      null: false
+    t.date     "end_date",                        null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "open_donation",   default: true,  null: false
+    t.integer  "dollar_to_point", default: 1,     null: false
+    t.boolean  "active",          default: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
