d0daaf059cf28b0c1d04299c1249b779a37962d6
Mark Nguyen <pnguyen1097@gmail.com>
2016-11-16 00:22:27 -0500

    Update vote button helper to be shorter.

--- app/helpers/vote_button_helpers.rb
+++ app/helpers/vote_button_helpers.rb
@@ -1,5 +1,4 @@
 module VoteButtonHelpers
-
   def link_to_vote_always_show(project, name = nil, html_options = nil, &block)
     link_to_vote(project, name, html_options, &block) ||
       capture(&(block || proc { concat name }))
@@ -18,18 +17,13 @@ module VoteButtonHelpers
   private
 
   def link_only(project, name = nil, html_options = nil, &block)
-    if block_given?
-      link_to new_transaction_path(project_id: project.id), html_options, &block
-    else
-      link_to name, new_transaction_path(project_id: project.id), html_options
-    end
+    html_options, block, name = name, html_options, capture(&block) if block_given?
 
+    link_to name, new_project_transaction_path(project), html_options
   end
 
   def link_with_modal(project, name = nil, html_options = nil, &block)
-
-    # mirror Rails's link_to
-    html_options, block, name = name, block, block if block_given?
+    html_options, block, name = name, html_options, capture(&block) if block_given?
 
     modal_id = "sms_vote_#{project.id}"
     html_options = html_options.merge({
@@ -38,11 +32,7 @@ module VoteButtonHelpers
     })
 
     capture do
-      if block_given?
-        concat link_to('#', html_options, &block)
-      else
-        concat link_to(name, '#', html_options)
-      end
+      concat link_to(name, '#', html_options)
       concat render partial: 'shared/sms_modal', locals: {
         modal_id: modal_id,
         project: project
