0e7c256f7bae1149e2834682de03976a6f81332c
Alex Vargas <vlexaargas@gmail.com>
2016-11-15 22:54:35 -0500

    Convert homepage to use internationalization for content.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     iframe = content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
     content_tag(:div, iframe, class: 'embed-container')
   end
+
+  def competition_scope
+    "competition_copy.#{@competition.code_name.parameterize.underscore}"
+  end
 end
